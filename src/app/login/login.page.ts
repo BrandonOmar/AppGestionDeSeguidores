a330f4987bba1ccef62d4a7ffc402efce6082ce9
BrandonOmar <77993401+BrandonOmar@users.noreply.github.com>
2021-02-02 19:42:06 -0600

    direccionamiento facebook

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -46,8 +46,8 @@ export class LoginPage implements OnInit {
   redirectUser(isVerified: boolean){
 
     if(isVerified){
-       location.href = "https://www.facebook.com/VolverASonarAC/";
-     // location.href = "http://www.vuelveasoñar.org";
+      // location.href = "https://www.facebook.com/VolverASonarAC/";
+     location.href = "https://www.facebook.com/VolverASonarAC/?hc_ref=ARRwwR7nGp65IcQDKrJw5b4ig2EJrqJMj1o0j9CaoYgb_VYPoKU-8Cjz2PtnvFgqgjg&fref=nf&__xts__[0]=68.ARB_Kz7eNWaw93EBas2BkpMQSFgnnb2smvaqyWeR46Bm2WtHkn3LXPj7ZRiGXp4O7mgxYJ2BaCR4P5HosoFFPtG6m7nq9d6uHeDO018xYwv88kGRk39tYxLlA1elMhL06BYASJ_IMjz_uEYentXV3OIUFmXmVwOj8osnxIdFUTovPmSUIf0hv-7uWDUCAA4xXeWc-klxSFXKdhawVK1bCSvbFCEtpU3Psvx6XA2zeN0NSI5ZOJpUPph8YKRAfuRUgqsQECiB_ST6S39bhUoKpHdwmag9gG--M2IK-zbu2UQw45MPPFMcAFHZR1K33MArzeo&__tn__=kC-R";
     }
 
   }
